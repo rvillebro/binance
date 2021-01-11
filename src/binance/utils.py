25a66ea7b80b5a8bf7e8f191fdb8fa004193650e
Rasmus Villebro <rasmus-villebro@hotmail.com>
2021-01-11 18:17:07 +0100

    add docstring to utils

--- src/binance/utils.py
+++ src/binance/utils.py
@@ -2,6 +2,15 @@
 import time
 
 def clean_params(params):
+    """
+    Removes all key-value pairs have a value of None
+
+    Args:
+        params (dict): parameter dictionary to clean
+    
+    Return:
+        dict: cleaned parameters dictionary
+    """
     cleaned_params = dict()
     for key, value in params.items():
         if value is not None:
@@ -11,5 +20,11 @@ def clean_params(params):
 
 
 def get_timestamp():
+    """
+    Gets timestamp in miliseconds
+
+    Return:
+        int: timestamp in miliseconds
+    """
     timestamp = int(round(time.time() * 1000))
     return timestamp
