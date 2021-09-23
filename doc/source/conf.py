0555efbadd70309299b43f3f0b4ce6ded58c1594
Rasmus Villebro <rasmus-villebro@hotmail.com>
2021-09-23 21:14:59 +0200

    test

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
-
 ]
 
 # Add any paths that contain templates here, relative to this directory.
